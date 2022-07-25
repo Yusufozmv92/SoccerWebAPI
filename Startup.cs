8a166a9ed5af6363cf40976b14cd1f06b4947745
Igor Voronetchi <ivoronetchi8@gmail.com>
2022-07-26 01:32:50 +0300

    Adding changes in appsettings.json, Program.cs, and migration files, and others

---
+++ Startup.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoccerWebAPI.Contexts;
+
+namespace SoccerWebAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+        public static void ConfigureServices(WebApplicationBuilder builder)
+        {
+            var connectionString = builder.Configuration["ConnectionStrings:TeamsDBConnectionString"];
+            builder.Services.AddDbContext<TeamsContext>(o => o.UseSqlServer(connectionString));
+            builder.Services.AddControllers();
+        }
+        public static void Configure(WebApplication app)
+        {
+            //Configure the HTTP request pipeline
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+
+        }
+    }
+}
