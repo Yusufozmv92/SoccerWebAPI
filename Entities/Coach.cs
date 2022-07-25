9253d7a2f2bf45169dc21005ad462648002210f7
Igor Voronetchi <ivoronetchi8@gmail.com>
2022-07-26 01:29:21 +0300

    Adding entities and contexts

---
+++ Entities/Coach.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SoccerWebAPI.Entities
+{
+    public class Coach
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        [MaxLength(150)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        public string LastName { get; set; }
+        public bool? Deleted { get; set; }
+    }
+
+}
